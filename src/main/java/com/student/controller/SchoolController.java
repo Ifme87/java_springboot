734d6ace27bd46b83e01d8cba4d08b615ce598d6
A200063839 <A200063839@192.168.1.2>
2023-10-25 12:43:49 +0300

    initial commit

---
+++ src/main/java/com/student/controller/SchoolController.java
@@ -0,0 +1,32 @@
+package com.student.controller;
+
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.student.core.College;
+import com.student.service.StudentService;
+
+@Controller
+@RequestMapping("/school")
+public class SchoolController {
+	
+	@Inject
+	StudentService studentService;
+	
+	@GetMapping
+	public ModelAndView getAllSchools() {
+		
+		Collection<College> colleges = 
+			studentService.getAllStudents().stream().map(p-> p.getCollege()).distinct().collect(Collectors.toList());
+		
+		return new ModelAndView("home", "colleges", colleges);
+	}
+
+}
